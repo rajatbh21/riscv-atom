d4ca917957b4e7cfa3dbcfd121c389a73e71b76d
Saurabh Singh <saurabh.s99100@gmail.com>
2022-01-17 19:51:04 +0530

    program.py now operates on section paradigm
    Program.py earlier used to send the contents of binary files while
    reading the file. In order to extend program.py for vhex files
    (verilog hex), it now operates on sections.
    
    file is parsed into a list of sections, a binary file has only
    one section while a vhex file may have many.
    
    This sections are then programmed one by one

--- sw/bootloader/program.py
+++ sw/bootloader/program.py
@@ -1,4 +1,6 @@
 from typing_extensions import Required
+from typing import List
+
 import serial
 import os
 import sys
@@ -18,38 +20,54 @@ class style():
 
 
 class AtomProgrammer:
+    version = '1.0'
+
     KEY_BYTE = [b'p']
     ACK_BYTE = b'.'
-    
-    BaseAddr = 0x000000400  # start of ROM
 
+    CMD_COPY = [b'c']
+    CMD_RST  = [b'r']
+    CMD_EXIT = [b'x']
+    
     class Baud:
         B_9600 = 9600
 
-    def __init__(self, port:str, baud:Baud, file:str, verbose_flag:bool=False, debug_flag:bool=False, txdelay:int=0.07):
+    allowed_input_formats = ['vhex', 'bin']
+
+    
+    class Section:
+        def __init__(self, base:int, size:int, contents:List[bytes]):
+            self.base = base
+            self.size = size
+            self.contents = contents
+
+
+    def __init__(self, port:str, baud:Baud, file:str, iformat:str, base_addr:int, verbose_flag:bool=False, debug_flag:bool=False, txdelay:int=0.05):
         ## initialize atom programmer
         self._port = port
         self._baud = baud
         self._file = file
+        self._iformat = iformat
+        self._base_addr = base_addr
         self._ser = None
         self._verbose_flag = verbose_flag
         self._debug_flag = debug_flag
         self._txdelay = txdelay
 
 
-    def _open_port(self):
+    def __open_port(self):
         ## open serial port
         if self._ser == None:
             self._ser = serial.Serial(self._port, self._baud)
 
 
-    def _close_port(self):
+    def __close_port(self):
         ## close serial port
         if self._ser != None:
             self._ser.close()
 
 
-    def _send_int(self, val:int):
+    def __send_int(self, val:int):
         ## Send integer
         for _ in range(4):
             byte = (val & 0x000000ff).to_bytes(1, 'little')
@@ -62,7 +80,7 @@ class AtomProgrammer:
             sleep(self._txdelay)
 
 
-    def _send_bytes(self, arr):
+    def __send_bytes(self, arr):
         ## Send bytes
         for b in arr:
             self._ser.write(b)
@@ -73,7 +91,7 @@ class AtomProgrammer:
             sleep(self._txdelay)
 
 
-    def _progress(self, count, total, prefix='', suffix=''):
+    def __progress(self, count, total, prefix='', suffix=''):
         ## Update program counter
         bar_len = 20
         filled_len = int(round(bar_len * count / float(total)))
@@ -85,25 +103,25 @@ class AtomProgrammer:
         sys.stdout.flush()  # As suggested by Rom Ruben
 
 
-    def _send_fcontents(self, file:str, sz:int):
-        ## Send contentes of file
-        with open(file, "rb") as f:
-            for i in range(sz):
-                byte = f.read(1)
-                self._ser.write(byte)
-                
-                if self._verbose_flag:
-                    perc = int(i*100/sz)
-                    if perc % 5 == 0:
-                        self._progress(i, sz, style.GREEN + 'Programming...        ' + style.RESET)
+    def __send_section(self, sec:Section):
+        for i in range(sec.size):
+            self._ser.write(sec.contents[i])
+            
+            if(self._debug_flag):
+                print('<< ', sec.contents[i])   
 
-                sleep(self._txdelay)
-        
-        if self._verbose_flag:
-            print('\t\tDone!')
+            if self._verbose_flag and not self._debug_flag:
+                perc = int(i*100/sec.size)
+                if perc % 5 == 0:
+                    self.__progress(i, sec.size)
+                    pass
+
+            sleep(self._txdelay)
+        print()
+        return True
 
 
-    def _wait_for_ack(self, timeout=2):
+    def __wait_for_ack(self)->bool:
         ## wait for acknowledgement from device
         byte = self._ser.read()
         
@@ -116,108 +134,107 @@ class AtomProgrammer:
             return False
 
     
-    def _exit(self, reason:str):
+    def __exit(self, reason:str):
         # exit programmer with error
         print(style.RED, '!ERROR: ', style.RESET, reason)
         sys.exit()
 
 
-    def program(self, base:int, exit=True):
+    def __parse_file(self) -> List[Section]:
+        if self._iformat == 'bin':
+            base = self._base_addr
+            contents = []
+
+            with open(self._file, "rb") as f:
+                byte = f.read(1)
+                while byte:
+                    contents+=[byte]
+                    byte = f.read(1)
+                    
+            return [self.Section(base, len(contents), contents)]
+        
+        elif self._iformat == 'vhex':
+            pass
+
+
+    def program(self, exit=True):
         ## program device
-        fsize = os.path.getsize(self._file)
+
+        # format [base_addr, size, [content]]
+        sections = self.__parse_file()
 
         if self._verbose_flag:
-            print('Port: ', self._port)
-            print('Baud: ', self._baud, 'bps\n')
+            print('Port:\t', self._port)
+            print('Baud:\t', self._baud, 'bps\n')
 
-            print('File: ', self._file)
-            print('Base: ', hex(base))
-            print('Size: ', fsize, 'bytes\n')
-            
+            print('File:\t', self._file)
+            for i in range(len(sections)):
+                print('\tSection['+str(i+1)+']\tbase: ', str.format('0x{:08X}', sections[i].base), '\tsize: ', sections[i].size, 'bytes')            
+            print()
 
-        if(self._verbose_flag):
-            print(style.GREEN+'Initializing...'+style.RESET, end='')
+        def start_task(task:str):
+            if(self._verbose_flag):
+                print(style.CYAN+'>> '+task+style.RESET)
+        
+        def start_subtask(task:str):
+            if(self._verbose_flag):
+                print(style.YELLOW+'- '+task+style.RESET)
+
+        def end_task(lines:int=1):
+            if(self._verbose_flag):
+                print('\033['+str(lines)+'A'+'\033[30C'+'Done!')
+
+        def check_ack():
+            if self.__wait_for_ack() == False:
+                self.__exit('ACK not recieved')          
+
 
         # ----- Open serial port -----
-        self._open_port()
-
-        if self._verbose_flag:
-            print('  Done!')
-        
+        start_task('Initiaizing...')
+        self.__open_port()
+        end_task()
 
         # ----- send key byte -----
-        if(self._verbose_flag):
-            print(style.GREEN+'Sending key byte...'+style.RESET, end='')
+        start_task('Sending key byte...')
+        self.__send_bytes(self.KEY_BYTE)
+        check_ack()
+        end_task()
         
-        self._send_bytes(self.KEY_BYTE)
-        if not self._wait_for_ack():
-            self._exit('ACK not recieved')
-        
-        if self._verbose_flag:
-            print('  Done!')
-
-        
-        # ----- send command -----
-        if(self._verbose_flag):
-            print(style.GREEN+'Sending copy command...'+style.RESET, end='')
-
-        self._send_bytes([b'c'])
-        if not self._wait_for_ack():
-            self._exit('ACK not recieved')
-        
-        if self._verbose_flag:
-            print('  Done!')
-        
-        
-        # ----- Send base address -----
-        if(self._verbose_flag):
-            print(style.GREEN+'Sending base address...'+style.RESET, end='')
-
-        self._send_int(base)
-        if not self._wait_for_ack():
-            self._exit('ACK not recieved')
-
-        if self._verbose_flag:
-            print('  Done!')
-
-
-        # ----- Send file size -----
-        if(self._verbose_flag):
-            print(style.GREEN+'Sent file size...'+style.RESET, end='')
-
-        self._send_int(fsize)
-        if not self._wait_for_ack():
-            self._exit('ACK not recieved')
-
-        if self._verbose_flag:
-            print('  Done!')
-       
-
-        # ----- send file contents -----
-
-        self._send_fcontents(self._file, fsize)
-        if not self._wait_for_ack():
-            self._exit('ACK not recieved')
-
-
-        # ----- exit programming mode -----
-        if exit:
+        # ----- program sections -----
+        for i in range(len(sections)):
+            s = sections[i]
+            start_task('Programming Section['+str(i)+']...')
             
-            if(self._verbose_flag):
-                print(style.GREEN+'Sending exit command...'+style.RESET, end='')
+            # send copy command
+            start_subtask('Sending copy command...')
+            self.__send_bytes(self.CMD_COPY)
+            check_ack()
+            
+            # send section base address
+            start_subtask('Sending section base address... ('+str.format('0x{:08X}', s.base)+')')
+            self.__send_int(s.base)
+            check_ack()
 
-            self._ser.write(b'x')
-            if not self._wait_for_ack():
-                self._exit('ACK not recieved')
+            # send section size
+            start_subtask('Sending section size... ('+str(s.size)+' bytes)')
+            self.__send_int(s.size)
+            check_ack()
 
-            if self._verbose_flag:
-                print('\tDone!')
+            # send section contents
+            start_subtask('Sending section contents...')
+            self.__send_section(s)
+            check_ack()
+            end_task()
+
+        start_task('Sending exit command...')
+        self.__send_bytes(self.CMD_EXIT)
+        check_ack()
+        end_task()
 
         # ----- Close serial port -----
-        self._close_port()
+        self.__close_port()
 
         return True
-
 
 
 if __name__ == "__main__":
@@ -226,17 +243,28 @@ if __name__ == "__main__":
 
     # Positional Arguments
     parser.add_argument("port", help="Serial port to which device is connected", type=str)
-    parser.add_argument("file", help="Binary image file", type=str)
+    parser.add_argument("file", help="Image file", type=str)
 
     # Optional Arguments
-    parser.add_argument("-a", "--address", help="Base address", type=int, default=AtomProgrammer.BaseAddr)
+    parser.add_argument("-f", "--format", help="Input format: "+str(AtomProgrammer.allowed_input_formats), type=str, default='bin')
+    parser.add_argument("-a", "--address", help="Base address (optionally required if format==bin)", type=int, default=0x00000400)
     parser.add_argument("-v", "--verbose", help="Increase output verbosity", action="store_true")
     parser.add_argument("-d", "--debug", help="Print debugging information", action="store_true")
     parser.add_argument("-b", "--baud", help="Set Uart Baudrate", type=int, default=AtomProgrammer.Baud.B_9600)
 
     args = parser.parse_args()
 
-    ap = AtomProgrammer(args.port, args.baud, args.file, args.verbose, args.debug)
 
-    if ap.program(args.address):
+    if args.verbose:
+        print(style.GREEN+'AtomProgrammer v'+AtomProgrammer.version+style.RESET)
+
+    if args.format not in AtomProgrammer.allowed_input_formats:
+        print(style.RED, 'Unsupported input format: ', style.RESET, args.format)
+        sys.exit()
+
+    # Instantiate programmer
+    ap = AtomProgrammer(args.port, args.baud, args.file, args.format, args.address, args.verbose, args.debug)
+
+    # Program
+    if args.verbose and ap.program():
         print(style.YELLOW, 'Programming Successful!!', style.RESET)
